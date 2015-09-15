9ce1c257a56e3eb2813577d56f5ace28d4683c4e
unknown <RobbieR@Robbie-Laptop.aeriandi.local>
2015-09-15 16:19:18 +0100

    configure LCM for TestVM1, TestVM2

---
+++ source/Set Local Configuration Manager/1_LCM_Push_TestVM1-2.ps1
@@ -0,0 +1,30 @@
+﻿##########################################################################################################################
+#Set Local Configuration Manager configuration mode from defaul (ApplyAndMonitor) to ApplyAndAutoCorrect
+#This method is the new way of setting the LCM, this configures the management agent
+#configures the meta data configuration and generates a xx.meta.mof which is deployed via set-dsclocalconfigurationmanger
+##########################################################################################################################
+
+# create fuction to update configuration mode and refresh mode for Variable $Computername
+[DSCLocalConfigurationManager()]
+Configuration LCMPUSH 
+{	
+	Node $Computername
+	{
+		SEttings
+		{
+			AllowModuleOverwrite = $True
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+			RefreshMode = 'Push'	
+		}
+	}
+}
+
+# Generates configuration for below Variable
+$Computername = 'TestVM1','TestVM2'
+
+# Creates the Computer.Meta.Mof in folder below
+LCMPush -OutputPath c:\DSC\LCM
+
+
+
+
